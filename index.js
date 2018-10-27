54a8f7d595000f9ae0562f82953033235a989d0e
Artem Kudla <kudla.artem@gmail.com>
2018-10-27 11:50:54 +0300

    promise status implemented

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const promiseStatus = require('./lib/promiseStatus');
+const promiseStatuses = require('./lib/promiseStatuses');
 
 Object.assign(
     module.exports,
-    promiseStatus
+    promiseStatus,
+    promiseStatuses
 );
--- test/lib/promiseStatus.test.js
+++ test/lib/promiseStatus.test.js
@@ -1,7 +1,33 @@
 const {promiseStatus} = require('../../lib/promiseStatus');
+const {
+    PROMISE_PENDING,
+    PROMISE_RESOLVED,
+    PROMISE_REJECTED
+} = require('../../lib/promiseStatuses');
 
 describe('lib/promiseStatus', () => {
     it('should be a function', () => {
         expect(promiseStatus).to.be.instanceof(Function);
+    });
+
+    it('should resovle pending status', async () => {
+        const result = await promiseStatus(new Promise(() => {}));
+        expect(result).to.deep.equal({status: PROMISE_PENDING});
+    });
+
+    it('should resovle resolved status', async () => {
+        const value = {some: 'value'};
+        const result = await promiseStatus(Promise.resolve(value));
+
+        expect(result, 'result structure').to.deep.equal({status: PROMISE_RESOLVED, value});
+        expect(result.value, 'resolved value').to.equal(value);
+    });
+
+    it('should resovle rejected status', async () => {
+        const reason = {some: 'value'};
+        const result = await promiseStatus(Promise.reject(reason));
+
+        expect(result, 'result structure').to.deep.equal({status: PROMISE_REJECTED, reason});
+        expect(result.reason, 'rejection reason').to.equal(reason);
     });
 });
