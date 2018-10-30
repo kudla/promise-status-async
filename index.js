21b306f42465e8cd50996433c0336394dc09b529
Artem Kudla <kudla.artem@gmail.com>
2018-10-30 19:56:40 +0200

    predicates export fix

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const promiseState = require('./lib/promiseState');
 const promiseStatus = require('./lib/promiseStatus');
 const promiseStatuses = require('./lib/promiseStatuses');
-const predicates = require('./lib/predicates');
+const promisePredicates = require('./lib/promisePredicates');
 
 Object.assign(
     module.exports,
     promiseState,
     promiseStatus,
     promiseStatuses,
-    predicates
+    promisePredicates
 );
