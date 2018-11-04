f4d7c9cb74912bf504a0cc3cc3a164f7c6c2ebf6
Artem Kudla <kudla.artem@gmail.com>
2018-11-04 16:46:12 +0200

    support for sync values

--- test/lib/promiseState.test.js
+++ test/lib/promiseState.test.js
@@ -10,12 +10,12 @@ describe('lib/promiseState', () => {
         expect(promiseState).to.be.instanceof(Function);
     });
 
-    it('should resovle pending state', async () => {
+    it('should resolve pending state', async () => {
         const state = await promiseState(new Promise(() => {}));
         expect(state).to.deep.equal({status: PROMISE_PENDING});
     });
 
-    it('should resovle resolved state', async () => {
+    it('should resolve resolved state', async () => {
         const value = {some: 'value'};
         const state = await promiseState(Promise.resolve(value));
 
@@ -23,11 +23,19 @@ describe('lib/promiseState', () => {
         expect(state.value, 'resolved value').to.equal(value);
     });
 
-    it('should resovle rejected state', async () => {
-        const reason = {some: 'value'};
+    it('should resolve rejected state', async () => {
+        const reason = {some: 'reason'};
         const state = await promiseState(Promise.reject(reason));
 
         expect(state, 'state structure').to.deep.equal({status: PROMISE_REJECTED, reason});
         expect(state.reason, 'rejection reason').to.equal(reason);
+    });
+
+    it('should resolve sync values', async () => {
+        const value = {some: 'value'};
+        const state = await promiseState(value);
+
+        expect(state, 'state structure').to.deep.equal({status: PROMISE_RESOLVED, value});
+        expect(state.value, 'resolved value').to.equal(value);
     });
 });
